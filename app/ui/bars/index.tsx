892b830f2b8a4fdab3e659dcee0f8916c2e47728
albert <albert@gradguard.com>
2025-05-22 12:53:03 -0400

    main

--- app/ui/bars/index.tsx
+++ app/ui/bars/index.tsx
@@ -6,7 +6,17 @@ interface BarsBarProps {
 const Bars = ({ bars }: BarsBarProps) => {
   return (
     <div>
-      {/* Here the bars will be rendered */}
+      {bars.map((bar) => (
+        <div
+          key={bar.title}
+          className="flex flex-row text-sm text-gray-500 divide-x divide-gray-200 border border-gray-200 rounded-md mb-2"
+        >
+          <div className="font-semibold p-2">{bar.title}</div>
+          <div className="p-2">{bar.currentValue}</div>
+          <div className="p-2">{bar.previousValue}</div>
+          <div className="p-2">{bar.color}</div>
+        </div>
+      ))}
     </div>
   );
 };
